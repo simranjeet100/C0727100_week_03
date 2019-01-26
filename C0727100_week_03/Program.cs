07887aedccf273ee74ec2842f8bac086918b1c06
visualstudio <visualstudio@122-PC22>
2019-01-25 21:40:02 -0500

    lastcommit

--- C0727100_week_03/Program.cs
+++ C0727100_week_03/Program.cs
@@ -6,27 +6,52 @@ using System.Threading.Tasks;
 
 namespace C0727100_week_03
 {
+  
     class Program
-    {
-        static void Main(string[] args)
         {
+            static void Main(string[]args)
+            {
+
+            }
+        }
+    class Countryside
+    {
+        Village Alst;
+        Village Maeland;
+        Village Schenig;
+
+        public void InitializeMap()
+        {
+            Alst = new Village();
+            Alst.VillageName = "Alst";
+            Maeland = new Village();
+            Maeland.VillageName = "Maeland";
+            Maeland.isAstrildeHere = true;
+            Schenig = new Village();
+            Schenig.VillageName = "Schenig";
+
+            Alst.NextVillage = Maeland;
+            Maeland.NextVillage = Schenig;
+            Schenig.NextVillage = Alst;
+        }
+
+        public void WalkAround()
+        {
+            Village CurrentVillage = this.Maeland;
+            Village NextVillage;
+
+            NextVillage = CurrentVillage.NextVillage;
 
 
         }
     }
-    class countryside
-    {
-        // how will i make some vilklage?
-
-    }
 
     class Village
     {
-        Village previousVillage;
-        Village NextVillage;
-        String villageName;
-        bool isAstrildeHere = false;
+        public Village PreviousVillage;
+        public Village NextVillage;
+        public String VillageName;
+        public bool isAstrildeHere = false;
         public Village() { }
-     
     }
 }
